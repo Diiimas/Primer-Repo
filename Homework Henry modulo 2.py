d05742e7e8ca939052c3e1398f919d6ab1431656
Diiimas <dimasgimenez@gmail.com>
2022-08-14 15:47:21 -0300

    segunda subida

---
+++ Homework Henry modulo 2.py
@@ -0,0 +1,37 @@
+from pickletools import decimalnl_short
+
+
+entero = 6
+print(entero)
+
+print(8.5)
+
+print(type entero)
+
+nombre = "Dimas"
+
+complejo = 1+6j
+
+type(complejo)
+
+pi = 3.1416
+
+verdadero = "True"
+
+verdadero_bool = True
+
+suma = 10 + 10.5
+
+suma_compleja = (10 + 6j) + (11+9j)
+
+suma_compleja_real = (8 + 4j) + 6
+
+2*3
+
+print(2**8)
+
+cociente = 24//7
+print(cociente)
+
+
+
